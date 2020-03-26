b0eb92faad55df32d0c756a1947c51bb625eab5b
eugene <zreek2001@gmail.com>
2020-03-26 23:22:22 +0200

    add main

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+from Shunting_yard import Shunting_yard
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('expression', help='arithmetic expression', nargs='+')
+    args = parser.parse_args()
+
+    expression = ''.join(args.expression)
+    replaced = expression.replace('**', '^')
+    check = eval(expression)
+
+    yard = Shunting_yard(replaced)
+    yard.replace_unary_minus()
+    yard.to_postfix()
+
+    print(f'Result: {yard.eval_postfix()}')
+    print(f'To check: {check}')
